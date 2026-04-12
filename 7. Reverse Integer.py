de2c1fe6a4eaf84081b03670e20ceff3d3e75a67
Your Name <ivan.safronov.karate@gmail.com>
2026-04-12 19:14:42 +0300

    modified:   7. Reverse Integer.py

--- 7. Reverse Integer.py
+++ 7. Reverse Integer.py
@@ -1,1 +1,10 @@
-ans
+class Solution:
+    def reverse(self, x: int) -> int:
+        if x == 0:
+            return 0
+        zn = x//abs(x)
+        ans = int(str(abs(x))[::-1])*zn
+        if ans >= (2**31)-1 or ans <= -2**31+1:
+            return 0
+        return ans
+        
